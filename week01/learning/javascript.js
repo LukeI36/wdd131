36976a56b2c980093ee794a98d2cdc4fa508d1cb
LukeI36 <LukeIna36@gmail.com>
2026-05-04 17:46:52 -0700

    Start to learn java part 1 week 1

---
+++ week01/learning/javascript.js
@@ -0,0 +1,21 @@
+/* 
+JavaScript is a popular, well-supported programming language used to create interactive and dynamic web pages, mobile apps, desktop applications, and more. It is a high-level, interpreted language designed to run in web browsers, and it can also be used on the server side with the help of technologies like Node.js.
+
+JavaScripts principles and code structure is similar to python and C#
+
+
+HTML is the structure of the website.
+CSS is the style/look of the website.
+JavaScript is the behavior of the website.
+
+To make a new JavaScript file, use .js after the file name
+
+
+JavaScript is able to add complex features to websites, such as displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc.
+
+We can make it so users interact with the website and the website will store the user's response.
+
+
+
+
+*/
