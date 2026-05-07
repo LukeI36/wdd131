42275365b2b258e5aa78c79e9949c0ce1833688e
LukeI36 <LukeIna36@gmail.com>
2026-05-07 15:49:46 -0700

    update 3 week 1 assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 const current_year = document.querySelector("#currentyear");
 const today = new Date()
 
-current_year.innerHTML = `${today.getFullYear()} `;
+current_year.innerHTML = `©${today.getFullYear()}`;
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
