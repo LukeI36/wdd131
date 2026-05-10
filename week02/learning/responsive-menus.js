1c5947477ff66c4c5091dd1669964f571931023c
LukeI36 <LukeIna36@gmail.com>
2026-05-09 18:56:34 -0700

    Week 2 5/6

---
+++ week02/learning/responsive-menus.js
@@ -0,0 +1,17 @@
+/*
+Responsive menus are menus that adapt to different viewpoint sizes and open up when clicking the hamburger button(≡)
+
+In responsive design, the goal is to give users a positive, familiar experience across devices.
+
+ex:
+https://codepen.io/BYU-Idaho/pen/yyBwGdO
+
+What does the :: syntax in the CSS rule selector do?
+The syntax selects a pseudo-elements to the selected element which in this case, allows us to add content to the existing content in the element.
+
+for mor info: https://blog.logrocket.com/css-pseudo-elements-guide/
+
+
+
+
+*/
