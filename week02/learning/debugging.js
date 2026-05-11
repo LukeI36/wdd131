1bd665c965480be7e6142c28f70d4979341d5d12
LukeI36 <LukeIna36@gmail.com>
2026-05-11 14:42:37 -0700

    Week 2 6/6

---
+++ week02/learning/debugging.js
@@ -0,0 +1,12 @@
+/*
+Debugging is the process of fixing errors in your code. It is essential to know when being any type of programmer.
+
+There are three types of errors when writing JavaScript:
+
+Syntax errors: When you write code that does not follow the rules of the JavaScript language. The browser detects these errors and will pervent your code from running.
+
+Runtime errors: occur when the code is running. Many things can cause a runtime error, like trying to access a property of an object that does not exist or calling a function that does not exist.
+
+Logical error: When the program produces a unexpected result. The code works but doesn't work in the way you attended. These errors can be hard to find because they do not cause the browser to throw an error.
+
+*/
