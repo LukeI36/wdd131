65f0577ada9b17cb394ff1e067add91f9532e143
LukeI36 <LukeIna36@gmail.com>
2026-05-13 17:07:07 -0700

    First Draft of Week 2 Assignment

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,18 @@
 const current_year = document.querySelector("#currentyear");
 const today = new Date()
 
+const ham_Button = document.getElementById("menu")
+const navigation = document.getElementById("navigation")
+
+//for hamburger button function
+ham_Button.addEventListener("click", function () {
+    navigation.classList.toggle("open");
+    ham_Button.classList.toggle("open");
+})
+
+
+//For footer modified time
 current_year.innerHTML = `${today.getFullYear()}`;
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+
