70e3ccb8aa95d96ecaa0de8f8432d600a0e65bf7
LukeI36 <LukeIna36@gmail.com>
2026-05-19 15:13:02 -0700

    keep working on Week 3 assignment

---
+++ scripts/place.js
@@ -0,0 +1,9 @@
+
+
+
+//footer code
+const current_year = document.querySelector("#currentyear");
+const today = new Date()
+
+current_year.innerHTML = `${today.getFullYear()}`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
