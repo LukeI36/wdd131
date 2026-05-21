ebcf7e2b7e72daf19e1bff2c39542a029f504d31
LukeI36 <LukeIna36@gmail.com>
2026-05-21 15:54:12 -0700

    Finished js of Week 3 assignment

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,28 @@
+//calculate wind
+const temperatureOutput = document.getElementById('celcius-temperature')
+const weatherOutput = document.getElementById('weather_condition')
+const windSpeedOutput = document.getElementById('wind-speed')
+const windChillOutput = document.getElementById('wind-chill')
 
+let temperature_C = 10;
+let weather = "Partly Cloudy";
+let wind_speed_per_hour = 5;
+
+function calculateWindChill(temp, wind) {
+    return 13.12 + 0.6215 * temp - 11.37 * (wind ** 0.16) + 0.3965 * temp * (wind ** 0.16);
+}
+
+if (temperature_C <= 10 && wind_speed_per_hour > 4.8) {
+    wind_chill = calculateWindChill(temperature_C, wind_speed_per_hour).toFixed(1);
+}
+else {
+    wind_chill = "N/A";
+}
+
+temperatureOutput.textContent = temperature_C;
+weatherOutput.textContent = weather;
+windSpeedOutput.textContent = wind_speed_per_hour;
+windChillOutput.textContent = wind_chill;
 
 
 //footer code
