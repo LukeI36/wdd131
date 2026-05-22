14c1bbd7cc5a52aad8a7a75e1776ddea803271f9
LukeI36 <LukeIna36@gmail.com>
2026-05-22 15:29:02 -0700

    Week 4 2/4

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,6 @@
+//footer code
+const current_year = document.querySelector("#currentyear");
+const today = new Date()
+
+current_year.innerHTML = `${today.getFullYear()}`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
