bb66d15576c324bbfc9635aacab620671cfd87b4
LukeI36 <LukeIna36@gmail.com>
2026-05-21 19:45:17 -0700

    Hopefully Final details for Week 3 assignment

--- scripts/place.js
+++ scripts/place.js
@@ -12,20 +12,21 @@ function calculateWindChill(temp, wind) {
     return 13.12 + 0.6215 * temp - 11.37 * (wind ** 0.16) + 0.3965 * temp * (wind ** 0.16);
 }
 
-if (temperature_C <= 10 && wind_speed_per_hour > 4.8) {
-    wind_chill = calculateWindChill(temperature_C, wind_speed_per_hour).toFixed(1);
-}
-else {
-    wind_chill = "N/A";
-}
-
 temperatureOutput.textContent = temperature_C;
 temperatureOutput.append(" °C");
 weatherOutput.textContent = weather;
 windSpeedOutput.textContent = wind_speed_per_hour;
 windSpeedOutput.append(" km/h");
-windChillOutput.textContent = wind_chill;
-windChillOutput.append(" °C");
+
+if (temperature_C <= 10 && wind_speed_per_hour > 4.8) {
+    wind_chill = calculateWindChill(temperature_C, wind_speed_per_hour).toFixed(1);
+    windChillOutput.textContent = wind_chill;
+    windChillOutput.append(" °C");
+}
+else {
+    wind_chill = "N/A";
+    windChillOutput.textContent = wind_chill;
+}
 
 
 //footer code
