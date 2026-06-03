1f08ceb22c8152644aa1b07b5a184bd1f13ea000
LukeI36 <LukeIna36@gmail.com>
2026-06-03 13:48:02 -0700

    continue working on week 5 assignment

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,47 @@
+//Select Product section 
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+function options() {
+    const productList = document.getElementById("product-name");
+    products.forEach(product => {
+        const fragment = document.createDocumentFragment();
+        const productOutput = document.createElement("option");
+        productOutput.setAttribute("value", `${product.id}`);
+        productOutput.textContent = `${product.name}`;
+        fragment.appendChild(productOutput);
+        productList.appendChild(fragment);
+    })
+}
+
+options();
+
+//Footer
 const current_year = document.querySelector("#currentyear");
 const today = new Date()
 
