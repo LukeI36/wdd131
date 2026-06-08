0025542ba287b87c0c59d4e4088bb6ea939b42e7
LukeI36 <LukeIna36@gmail.com>
2026-06-08 13:43:02 -0700

    another quick update to menu

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,8 +1,10 @@
 // Menu
+const top_menu = document.getElementById("top-menu")
 const ham_Button = document.getElementById("menu")
 const navigation = document.getElementById("navigation")
 
 ham_Button.addEventListener("click", function () {
+    top_menu.classList.toggle("open")
     navigation.classList.toggle("open");
     ham_Button.classList.toggle("open");
 })
